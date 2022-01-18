ec766c204a494054c7f3b0f1ae92385b8ae25a5b
noordseoostende <voropayev1@mail.ru>
2022-01-18 14:51:39 +0100

    new compnnt

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import SearchInput from './SearchInput';
+import axios from 'axios'
 
 class App extends React.Component {
-  constructor(){
+  constructor(props){
+    
     super(props)
+    this.state = {images: [] }
     this.onSearchSubmit = this.onSearchSubmit.bind(this)
-  }
-  onSearchSubmit(entry) {
-
+}
+  async onSearchSubmit(entry) {
+    // console.log(entry)
+    const response = await axios.get(`https://pixabay.com/api/?key=20727856-d52644f8c0aa870aa808665f1&q=${entry}&image_type=photo`)
+    console.log(response.data.hits)
+    this.setState({images: response.data.hits})
   }
   render() {
  
     return (
-      <div className='ui container' style={{marginTop: '30px'}}><SearchInput onSearchSubmit={this.onSearchSubmit} /></div>
+      <div className='ui container' style={{marginTop: '30px'}}><SearchInput onSearchSubmit={this.onSearchSubmit} />
+      We have {this.state.images.length} images</div>
       
       )}
   }
